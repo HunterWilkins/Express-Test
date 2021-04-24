83de1daff9d8dbf9d057227ae159fa49a52454f5
HunterWilkins <wilkins.hunter@gmail.com>
2021-04-24 11:34:40 -0500

    Added port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
-const PORT = 3001;
+const PORT = 3001 || process.env.PORT;
 
 // Data
 // ===========================================================
